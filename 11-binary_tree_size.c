f4cdb8add3c1c0c1b3043d1724bd230cb7bbc97d
tarie100 <tarie.school@gmail.com>
2023-11-28 03:57:38 -0800

    fix 1

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,9 @@
+#include"binary_trees"
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree:  pointer to the root node 
+ * Return: 0
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+
