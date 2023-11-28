a83044ebc75bfecf6fd30c471cb1132f31e7d5fb
serenalilian <serenalilian99@gmail.com>
2023-11-28 14:21:13 -0800

    function that checks if a binary tree is perfect

--- binary_trees.h
+++ binary_trees.h
@@ -28,4 +28,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 #endif /* BINARY_TREES_H */
