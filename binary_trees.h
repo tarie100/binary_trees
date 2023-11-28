1d5607727b9003508b5d4cd196f031d60967d36c
serenalilian <serenalilian99@gmail.com>
2023-11-28 13:45:51 -0800

    function that inserts a node as the right-child of another node

--- binary_trees.h
+++ binary_trees.h
@@ -14,6 +14,8 @@ typedef struct binary_tree_s
 
 /* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 void binary_tree_delete(binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
