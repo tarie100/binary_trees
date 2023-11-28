0d52928f0590255867b1f5eb01d0650058a4b44c
serenalilian <serenalilian99@gmail.com>
2023-11-28 14:13:00 -0800

    function that counts the leaves in a binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -26,5 +26,5 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_depth(const binary_tree_t *tree);
-
+size_t binary_tree_leaves(const binary_tree_t *tree);
 #endif /* BINARY_TREES_H */
