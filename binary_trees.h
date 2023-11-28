60afaf181ee4116b571a1524f91bed6a97950075
serenalilian <serenalilian99@gmail.com>
2023-11-28 14:08:48 -0800

    function that measures the depth of a node in a binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -25,4 +25,6 @@ size_t binary_tree_height(const binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_depth(const binary_tree_t *tree);
+
 #endif /* BINARY_TREES_H */
