78c66ab89939e68a321e6603bebeb2be27ec87d6
tarie100 <tarie.school@gmail.com>
2023-11-29 11:07:21 -0800

    siblings of node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,7 +6,7 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node->parent == NULL || node == NULL)
+	if (node == NULL || node->parent == NULL)
 	{
 		return (NULL);
 	}
--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -4,12 +4,13 @@
  * @tree: pointer to the root node
  * @func: pointer to a function to call for each node
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int)) {
-    if (tree == NULL || func == NULL) {
-        return;
-    }
-
-    binary_tree_inorder(tree->left, func);
-    func(tree->n);
-    binary_tree_inorder(tree->right, func);
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
