13322cab910fd19e2283755390edb8d700ceb7a5
tarie100 <tarie.school@gmail.com>
2023-11-29 11:21:16 -0800

    fixing betty errors

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,7 +1,7 @@
 #include"binary_trees.h"
 /**
  * binary_tree_size - measures the size of a binary tree
- * @tree: pointer to the root node 
+ * @tree: pointer to the root node
  * Return: 0
  */
 size_t binary_tree_size(const binary_tree_t *tree)
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,4 @@
-#include"binary_trees"
+#include"binary_trees.h"
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node
