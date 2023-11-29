c62c06abc6a9e9a1e2b1989bd718e245579889ab
tarie100 <tarie.school@gmail.com>
2023-11-29 11:00:04 -0800

    9-height

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,12 +1,11 @@
 #include"binary_trees.h"
 /**
  * binary_tree_size - measures the size of a binary tree
- * @tree:  pointer to the root node 
+ * @tree: pointer to the root node 
  * Return: 0
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-<<<<<<< HEAD
 	size_t size = 0;
 
 	if (tree)
@@ -16,11 +15,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		size += binary_tree_size(tree->right);
 	}
 	return (size);
-=======
 	if (tree == NULL)
 	{
-		return 0;
+		return (0);
 	}
-	return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
->>>>>>> a894451be87f9ee9afd96f81ddc62da33b10aa44
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,9 +9,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height;
 	size_t right_height;
 	
-	if (tree != NULL)
+	if (tree)
 	{
 		left_height = binary_tree_height(tree->left);
 		right_height = binary_tree_height(tree->right);
-		return 1 + (left_height > right_height ? left_height : right_height);
+		return (1 + (left_height > right_height ? left_height : right_height));
 	}
