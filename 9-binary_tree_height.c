09f9b5c9aebcdb79f03e95af2ceb5d374b87a049
tarie100 <tarie.school@gmail.com>
2023-11-29 11:24:00 -0800

    9-height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,3 +15,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		right_height = binary_tree_height(tree->right);
 		return (1 + (left_height > right_height ? left_height : right_height));
 	}
+	return (0);
+}
