8d645d6892e4876070d3dc796256d948d7cac4b6
serenalilian <serenalilian99@gmail.com>
2023-11-30 14:32:22 -0800

    function that performs a left-rotation on a binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -38,4 +38,5 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 int binary_tree_is_descendant(const binary_tree_t *ancestor, const binary_tree_t *node);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 #endif /* BINARY_TREES_H */
