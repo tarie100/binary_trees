33bf1e3bc0599cc039eb6b5f76bad8c0ae58edb8
serenalilian <serenalilian99@gmail.com>
2023-11-30 13:44:20 -0800

    function that goes through a binary tree using level-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -36,4 +36,5 @@ size_t binary_tree_size(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 int binary_tree_is_descendant(const binary_tree_t *ancestor, const binary_tree_t *node);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 #endif /* BINARY_TREES_H */
